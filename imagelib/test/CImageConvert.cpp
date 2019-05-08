f2fdb7cfd4daae111cc935f14719216aa1263075
hougl <hgl868@126.com>
2019-05-08 15:49:15 +0800

    修改编译

--- imagelib/test/CImageConvert.cpp
+++ imagelib/test/CImageConvert.cpp
@@ -8,56 +8,57 @@
 int
 main(int argc, char **argv)
 {
-  bool debug = false;
+    bool debug = false;
 
-  std::string ifilename;
-  std::string ofilename;
+    std::string ifilename;
+    std::string ofilename;
 
-  for (int i = 1; i < argc; ++i) {
-    if (argv[i][0] == '-') {
-      if      (strcmp(&argv[i][1], "debug") == 0)
-        debug = true;
-      else if (strcmp(&argv[i][1], "bg") == 0) {
-        ++i;
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-') {
+            if      (strcmp(&argv[i][1], "debug") == 0)
+                debug = true;
+            else if (strcmp(&argv[i][1], "bg") == 0) {
+                ++i;
 
-        if (i < argc)
-          CImage::setConvertBg(CRGBName::toRGBA(argv[i]));
-        else
-          std::cerr << "Missing value for -bg";
-      }
-      else
-        std::cerr << "Invalid option " << argv[i] << std::endl;
+                if (i < argc)
+                    CImage::setConvertBg(CRGBName::toRGBA(argv[i]));
+                else
+                    std::cerr << "Missing value for -bg";
+            }
+            else
+                std::cerr << "Invalid option " << argv[i] << std::endl;
+        }
+        else {
+            if (ifilename == "")
+                ifilename = argv[i];
+            else if (ofilename == "")
+                ofilename = argv[i];
+            else
+                std::cerr << "Invalid argument " << argv[i] << std::endl;
+        }
     }
-    else {
-      if      (ifilename == "")
-        ifilename = argv[i];
-      else if (ofilename == "")
-        ofilename = argv[i];
-      else
-        std::cerr << "Invalid argument " << argv[i] << std::endl;
+
+    if (ifilename == "" || ofilename == "") {
+        std::cerr << "Usage: CImageConvert [-debug] <ifile> <ofile>" << std::endl;
+        exit(1);
     }
-  }
 
-  if (ifilename == "" || ofilename == "") {
-    std::cerr << "Usage: CImageConvert [-debug] <ifile> <ofile>" << std::endl;
-    exit(1);
-  }
+    CFile ifile(ifilename);
+    CFile ofile(ofilename);
 
-  CFile ifile(ifilename);
-  CFile ofile(ofilename);
+    CImageState::setDebug(debug);
 
-  CImageState::setDebug(debug);
+    CImageFileSrc src(ifile);
 
-  CImageFileSrc src(ifile);
+    CImagePtr image = CImageMgrInst->createImage(src);
+//  CImagePtr img = image->resize(100, 100);
 
-  CImagePtr image = CImageMgrInst->createImage(src);
+    if (! image.isValid())
+        exit(1);
 
-  if (! image.isValid())
-    exit(1);
+    CFileType type = CFileUtil::getImageTypeFromName(ofilename);
 
-  CFileType type = CFileUtil::getImageTypeFromName(ofilename);
+    image->write(&ofile, type);
 
-  image->write(&ofile, type);
-
-  exit(0);
+    exit(0);
 }
