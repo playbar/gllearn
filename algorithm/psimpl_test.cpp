67460b0a6a145982809357286492f4293e0fb0c6
hougl <hgl868@126.com>
2019-08-02 15:53:16 +0800

    modify code

--- algorithm/psimpl_test.cpp
+++ algorithm/psimpl_test.cpp
@@ -4,15 +4,21 @@
 #include <list>
 #include "psimpl.h"
 
+
 int main(int argc, char** argv)
 {
-    unsigned tolerance = 25;     // point count tolerance
-    std::list <long> polyline;   // original polyline, assume not empty
-    std::vector <double> result; // resulting simplified polyline
+    unsigned tolerance = 30;     // point count tolerance
+    std::vector <double > polyline;     // original polyline, assume not empty
+    std::vector <double> result;        // resulting simplified polyline
+
+    for (int i = 0; i < 100 ; ++i) {
+        polyline.push_back( i * 5 );
+        polyline.push_back( i % 5 );
+    }
 
 // simplify the 4d polyline to 25 points
-    psimpl::simplify_douglas_peucker_n <4> (
-            polyline.begin (), polyline.end (),
+    psimpl::simplify_douglas_peucker_n <2> (
+            polyline.begin (), polyline.end(),
             tolerance, std::back_inserter (result));
 
     return 0;
